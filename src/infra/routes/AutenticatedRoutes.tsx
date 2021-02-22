ac67af01cc41e2ee9f70d4496f1e19b8a574f152
Patrick Moreira <ptkdev@outlook.com>
2021-02-22 09:58:44 -0300

    teste

--- src/infra/routes/AutenticatedRoutes.tsx
+++ src/infra/routes/AutenticatedRoutes.tsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Route } from 'react-router-dom'
 import Home from '../../views/templates/home'
+import Controle from '../../views/templates/rotasProtegidas/controle'
 
 function AutenticatedRoutes() {
   return (
     <BrowserRouter>
-      <Route path="/" component={ Home } />
+      <Route path="/" exact component={ Home } />
+      <Route path="/controlereg" component={ Controle } />
     </BrowserRouter>
   )
 }
---
+++ src/views/templates/rotasProtegidas/controle/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+export default function Controle() {
+  return (
+    <>
+      <h1>COntrole</h1>
+      <h2>teste12</h2>
+    </>
+  )
+}
